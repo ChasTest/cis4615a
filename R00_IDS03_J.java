00a47128d8ed632cd25cf67a30dc3e024bba6862
ChasKane <LawrenceCKane@gmail.com>
2019-11-02 18:00:39 -0400

    sanitize input to logger

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -6,13 +6,20 @@
  *
  ******************************************************************************/
 import java.util.logging.*;
+import java.util.regex.Pattern;
 
 public class R00_IDS03_J {
   public static void main(String[] args) {
     String s = "x\ny";
     Logger logger =
       Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    logger.log(Level.INFO, s);
+    logger.log(Level.INFO, sanitize(s));
   }
+
+  public static String sanitize(String username) {
+    return Pattern.matches("[A-Za-z0-9_]+", username)
+      ? username : "bad input";
+  }
+
 
 }
