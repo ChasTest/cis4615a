63ad2fe015b41d3e71d5d9b73c851c12ffc74ca6
ChasKane <LawrenceCKane@gmail.com>
2019-11-02 17:47:44 -0400

    init (also add .gitignore for .class files)

---
+++ R00_IDS03_J.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *  Compilation:  javac R06_Met01_J.java
+ *  Execution:    java R06_Met01_J
+ *
+ *  Calls function getAbsAdd(x,y) with legal and illegal values
+ *
+ ******************************************************************************/
+import java.util.logging.*;
+
+public class R00_IDS03_J {
+  public static void main(String[] args) {
+    String s = "x\ny";
+    Logger logger =
+      Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    logger.log(Level.INFO, s);
+  }
+
+}
