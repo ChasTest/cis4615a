5b83439f80fbd8eadf941528ccdcbd2c9bcdd389
ChasKane <LawrenceCKane@gmail.com>
2019-11-02 17:32:07 -0400

    now throws error

--- R06_Met01_J.java
+++ R06_Met01_J.java
@@ -29,11 +29,12 @@ public class R06_Met01_J {
    *Rule 06-Met01
    */
   public static int getAbsAdd(int x, int y) {
-    assert x != Integer.MIN_VALUE;
-    assert y != Integer.MIN_VALUE;
+    if(x == Integer.MIN_VALUE || y == Integer.MIN_VALUE)
+      throw new IllegalArgumentException();
     int absX = Math.abs(x);
     int absY = Math.abs(y);
-    assert(absX <= Integer.MAX_VALUE - absY);
-    return absX + absY ;
+    if(absX > Integer.MAX_VALUE - absY)
+      throw new IllegalArgumentException();
+    return absX + absY;
   }
 }
