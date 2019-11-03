c9b64b09d85e49df5fcea3c2a81092c7f37e943d
ChasKane <LawrenceCKane@gmail.com>
2019-11-02 20:42:59 -0400

    handle failure to delete file

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -3,7 +3,10 @@ import java.io.*;
 public class R02_XP00_J {
   public static void main(String[] args) {
     File someFile = new File ("someFileName.txt");
-    someFile.delete();
+    if (!someFile.delete()) {
+      System.out.println("couldn't delete");
+      // Handle failure to delete the file
+    }
   }
 
 }
