c023c3e13679660166f282f3c15e50b2bccb9473
ChasKane <LawrenceCKane@gmail.com>
2019-11-11 17:59:53 -0500

    now type safe

--- R05_OBJ03_J.java
+++ R05_OBJ03_J.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 class R05_OBJ03_J {
-  private static void addToList(List list, Object obj) {
-    list.add(obj); // Unchecked warning
+  private static void addToList(List<String> list, String str) {
+    list.add(str); // Unchecked warning
   }
 
   public static void main(String[] args) {
     List<String> list = new ArrayList<String> ();
-    addToList(list, 42);
+    addToList(list, "42");
     System.out.println(list.get(0));  // Throws ClassCastException
   }
 }
