94285f9372b09d8fcebf8e97c2afad8fe282168b
ChasKane <LawrenceCKane@gmail.com>
2019-11-10 23:23:14 -0700

    file has bad permissions

---
+++ R13_FIO01_J.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class R13_FIO01_J {
+  public static void main(String[] args) {
+    try {
+      Writer out = new FileWriter("file");
+    } catch(Exception e) {
+
+    }
+  }
+}
